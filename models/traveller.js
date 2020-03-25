0bcb4db29d8a4ff776bb1a76df38f911cf3f6c8c
Steven Burrows <steven@Stevens-MacBook-Air.local>
2020-03-25 22:17:06 +0000

    finshed all including extensions

--- models/traveller.js
+++ models/traveller.js
@@ -3,27 +3,36 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  const startLocations = this.journeys.map((journey) => journey.startLocation);
+  return startLocations
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  const endLocations = this.journeys.map((journey) => journey.endLocation);
+  return endLocations
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  const byTransport = this.journeys.filter((journey) => journey.transport === transport);
+  return byTransport
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  const overDistance = this.journeys.filter((journey) => journey.distance > minDistance);
+  return overDistance
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+return this.journeys.reduce((total, journey) => {
+  return total + journey.distance
+}, 0 )
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  const transportMethods = this.journeys.map((journey) => journey.transport);
+  const set = new Set(transportMethods);  
+  const unique = Array.from(set);
+  return unique
 };
 
 
